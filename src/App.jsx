84b29ea347c177eec7954f2700b8e62281a029f2
Nishant-Chaudhary5338 <nishantchaudhary5338@gmail.com>
2022-07-29 16:20:33 +0530

    initial commit, project setup with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="bg-red-500"> hello world</h1>
+    </div>
+  );
+}
+
+export default App;
